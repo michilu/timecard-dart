3d7c0edbd2dfb40722e5c9d4a2d11d2e5d045ff8
ENDOH takanao <djmchl@gmail.com>
2014-04-07 13:32:38 +0630

    send feedback on background

--- lib/component/feedback.dart
+++ lib/component/feedback.dart
@@ -1,5 +1,7 @@
 library feedback;
 
+import "dart:html";
+
 import "package:angular/angular.dart";
 import 'package:angular_ui/modal/modal.dart';
 
@@ -16,15 +18,17 @@ class FeedbackFormConfig {
 class FeedbackComponent {
 
   FeedbackFormConfig _config;
+  Http _http;
   Modal modal;
   ModalInstance modalInstance;
   Scope scope;
+  String description;
 
   String get action_url {
     return "https://docs.google.com/spreadsheet/formResponse?formkey=${_config.feedback_formkey}";
   }
 
-  FeedbackComponent(this.modal, this.scope, this._config);
+  FeedbackComponent(this._config, this._http, this.modal, this.scope);
 
   void open(String templateUrl) {
     modalInstance = modal.open(new ModalOptions(templateUrl:templateUrl), scope);
@@ -37,7 +41,34 @@ class FeedbackComponent {
       });
   }
 
-  void ok(sel) {
-    modalInstance.close(sel);
+  void ok(event) {
+    modalInstance.close(event);
+  }
+
+  void submit(event) {
+    var queryParameters = {
+      "entry.0.single": description,
+      "entry.1.single": "",
+      "entry.2.single": window.location.href
+    };
+    String data = new Uri.http(""/*authority*/, ""/*unencodedPath*/, queryParameters).query;
+    _http.post(action_url, data, headers: {
+      "content-type": "application/x-www-form-urlencoded; charset=UTF-8"
+    }).then((_response) {
+      ok(event);
+      description = "";
+    });
+  }
+}
+
+@NgDirective(
+    selector: "[feedback-form-background]"
+)
+class FeedbackFormBackground implements NgAttachAware{
+  Element element;
+
+  FeedbackFormBackground(this.element);
+
+  attach() {
   }
 }
